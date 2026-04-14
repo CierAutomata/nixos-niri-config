0d46ba51c46d0410244cdc4d93ea0fab0177f787
Cier <CierAutomata@pm.me>
2026-04-14 16:20:47 +0200

    vim.lsp.config für nvim

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -1,57 +1,85 @@
 vim.g.mapleader = " "
+
+-- 1. Lazy.nvim Bootstrapping
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- 2. Plugin Setup
 require("lazy").setup({
+  -- UI
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  { "neovim/lspconfig" },
-  { "hrsh7th/nvim-cmp", dependencies = { "hrsh7th/cmp-nvim-lsp", "L3MON4D3/LuaSnip" } },
-  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-  { "nvim-tree/nvim-tree.lua", dependencies = "nvim-tree/nvim-web-devicons" },
   { "nvim-lualine/lualine.nvim" },
+  { "nvim-tree/nvim-tree.lua", dependencies = "nvim-tree/nvim-web-devicons" },
+  
+  -- Tools
   { "nvim-telescope/telescope.nvim", dependencies = "nvim-lua/plenary.nvim" },
   { "lewis6991/gitsigns.nvim" },
-  {
-      "neovim/nvim-lspconfig",
-      dependencies = {
-        "williamboman/mason.nvim", -- Falls du Mason nutzt
-        "williamboman/mason-lspconfig.nvim",
-      },
-      config = function()
-        -- Wir nutzen pcall (protected call), damit nvim nicht abstürzt,
-        -- falls das Plugin mal wirklich nicht da ist.
-        local ok, lspconfig = pcall(require, "lspconfig")
-        if not ok then return end
+  
+  -- Treesitter
+  { 
+    "nvim-treesitter/nvim-treesitter", 
+    build = ":TSUpdate",
+    config = function()
+      require'nvim-treesitter.configs'.setup { 
+        highlight = { enable = true },
+        ensure_installed = { "nix", "lua" } 
+      }
+    end
+  },
 
-        -- Hier deine Server konfigurieren
-        -- Beispiel für Nix (nil_ls):
-        lspconfig.nil_ls.setup({})
-        
-        -- Beispiel für Python:
-        lspconfig.pyright.setup({})
-      end,
+  -- LSP & Completion
+  { "hrsh7th/nvim-cmp", dependencies = { "hrsh7th/cmp-nvim-lsp", "L3MON4D3/LuaSnip" } },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
     },
+    config = function()
+      -- Der neue Standard (Neovim 0.11+) nutzt intern vim.lsp.config
+      -- Wir konfigurieren hier nil_ls (Nix) und pyright (Python)
+      
+      -- nil_ls Setup
+      if vim.lsp.config then
+        -- Moderne Syntax (0.11+)
+        vim.lsp.config["nil_ls"] = {
+          setup = {},
+        }
+        vim.lsp.config["pyright"] = {
+          setup = {},
+        }
+      else
+        -- Fallback für 0.10.x
+        local lspconfig = require('lspconfig')
+        lspconfig.nil_ls.setup{}
+        lspconfig.pyright.setup{}
+      end
+    end,
+  },
 })
 
--- Appearance
+-- 3. Appearance Settings
 require("catppuccin").setup({ flavour = "mocha", transparent_background = true })
 vim.cmd.colorscheme "catppuccin"
 require('lualine').setup { options = { theme = 'catppuccin' } }
 
--- Functionality
+-- 4. General Options & Keymaps
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.termguicolors = true
+
 require("nvim-tree").setup()
 vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>')
 
--- LSP & Treesitter
-local lspconfig = require('lspconfig')
-lspconfig.nil_ls.setup{}
-require'nvim-treesitter.configs'.setup { 
-  highlight = { enable = true },
-  ensure_installed = { "nix", "lua" } 
-}
+-- Das "require('lspconfig')" ganz am Ende haben wir gelöscht, 
+-- da die Konfiguration jetzt sicher innerhalb des Lazy-Blocks stattfindet.
