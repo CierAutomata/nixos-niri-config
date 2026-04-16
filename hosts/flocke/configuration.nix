2a5443332a2a282cd918aa0e36ee0ab564508b2b
Cier <CierAutomata@pm.me>
2026-04-16 13:30:43 +0200

    test

--- hosts/flocke/configuration.nix
+++ hosts/flocke/configuration.nix
@@ -17,7 +17,12 @@
     isNormalUser = true;
     description = "Hauptbenutzer";
     extraGroups = [ "wheel" "networkmanager" "video" "disk" "storage" ];
-    hashedPasswordFile = config.sops.secrets.users.cier.hashedPassword.path;
+    hashedPasswordFile = if config.sops ? secrets &&
+      config.sops.secrets ? users &&
+      config.sops.secrets.users ? cier &&
+      config.sops.secrets.users.cier ? hashedPassword
+    then config.sops.secrets.users.cier.hashedPassword.path
+    else null;
   };
 
   users.mutableUsers = false;
